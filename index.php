996e66fc349e9dcdfabd53b8162d02f9c56ab2ca
danialessi <daniele.alessi@hotmail.it>
2021-05-19 16:04:29 +0200

    modifiche

--- index.php
+++ index.php
@@ -17,14 +17,17 @@
         ],
         [
             'question' => 'Perché il mio account è associato a un paese?',
-            'answer' => 'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:<br><br>'
+            'answer' => 'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+            <ol>
+                <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+                    <ol>
+                        <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+                    </ol>
+                </li>
+            </ol>'
         ]
     ];
 
-    // foreach($faq as $item) {
-    //     echo($item['question']);
-    //     echo($item['answer']);
-    // }
 ?>
 
 
